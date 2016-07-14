a503fd88eb509546550c8dba68a08be0ad88d50e
Christopher Rubio <ntashi.x3@gmail.com>
2016-07-13 23:02:46 -0300

    init commit

---
+++ models/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# importa todos los modelos en este __init__.py
+# http://stackoverflow.com/a/859222
+#
+# {{ app_name }}/models/__init__.py
+# from model_example import ModelExample
+#
+# {{ app_name }}/models/model_example.py
+# class ModelExample(models.Model):
+#    example_attr = models.CharField(max_length=128)
+#
+#    class Meta:
+#        app_label = '{{ app_name }}'
